29beef70be3138a17f1aa59eb7ea752687da4840
Giannina Bernal <bri.gia.bena@gmail.com>
2020-07-09 14:59:25 -0500

    Método para determinar la aprobación

---
+++ aprobado.py
@@ -0,0 +1,9 @@
+#Ejercicio que determine si una estudiante  esta aprobada  o no.
+#Autora: Giannina Bernal Navarrete
+
+def determinaraprobado(promedio):
+    if promedio >= 11:
+        resultado = "Aprobado"
+    else:
+        resultado = "Desaprobado"
+    return resultado

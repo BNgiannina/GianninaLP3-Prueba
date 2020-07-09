d994a452387bc85e510e11bd7b6eeaf03f3b8ee8
Giannina Bernal <bri.gia.bena@gmail.com>
2020-07-09 15:12:14 -0500

    Ejercicio de aprobados

--- aprobado.py
+++ aprobado.py
@@ -2,8 +2,11 @@
 #Autora: Giannina Bernal Navarrete
 
 def determinaraprobado(promedio):
+    
     if promedio >= 11:
         resultado = "Aprobado"
     else:
         resultado = "Desaprobado"
     return resultado
+promedio = int(input('Ingrese le promedio: '))
+print(determinaraprobado(promedio))
